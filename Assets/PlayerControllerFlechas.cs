017d106398dde3554c35c85b23347b3eaea698a4
Jeremias_Abulafia <jeremiasabulafia@gmail.com>
2022-06-01 15:56:15 -0300

    Initial commit

---
+++ Assets/PlayerControllerFlechas.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerFlechas : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+        transform.Translate(0,0,movementSpeed);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+        transform.Translate(0,0,-movementSpeed);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+        transform.Rotate(0,rotationSpeed,0);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+        transform.Rotate(0,-rotationSpeed,0);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Juego-Autos
+ 
