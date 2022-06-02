1e9314934026ef625c4086c154b8a88bb87ea452
GuidoZyl <guidozyl@gmail.com>
2022-06-02 20:18:59 -0300

    Bugs Fixed

---
+++ Assets/CorrectorLevitacion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectorLevitacion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y != 0.5f)
+        {
+            transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
+        }
+
+        if (transform.eulerAngles.x != 0)
+        {
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+
+        if (transform.eulerAngles.z != 0)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
+        }
+    }
+}
