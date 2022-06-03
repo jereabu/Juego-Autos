d7a6c84a0d9f9073c9835e01a48e5125e0690694
GuidoZyl <guidozyl@gmail.com>
2022-06-02 21:30:07 -0300

    added mortality

---
+++ Assets/ActualizarVidaFlechas.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActualizarVidaFlechas : MonoBehaviour
+{
+    public GameObject player;
+    int vidaFlechas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaFlechas = 100;       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vidaFlechas < 10)
+        {
+            Destroy(player);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "ArmaWASD")
+        {
+            vidaFlechas -= 10;
+            Debug.Log("Le diste");
+        }
+    }
+}
---
+++ Assets/ActualizarVidaWASD.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActualizarVidaWASD : MonoBehaviour
+{
+    public GameObject player;
+    int vidaWASD;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaWASD = 100;       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vidaWASD < 10)
+        {
+            Destroy(player);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "ArmaFlechas")
+        {
+            vidaWASD -= 10;
+            Debug.Log("Le diste");
+        }
+    }
+}
